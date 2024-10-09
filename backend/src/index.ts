2023c35416a94184789f0a5c9c884292f616f7a7
Raimisjes <raimisjes@gmail.com>
2024-10-09 09:03:26 +0200

    config fixes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const dotenv = require('dotenv').config({ path: "./.env" });
+
 app.use("/auth", authRoutes);
 app.use("/categories", categoryRoutes);
 app.use("/businesses", businessRoutes);
